84a4663abb2033ac63e2d350fc83efad83b94da7
Wesley Kellar <Wesley.Kellar@triosstudent.com>
2018-12-31 12:20:36 -0500

    Add project files.

---
+++ FredServer/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using CamTheGeek.GpioDotNet;
+using ManagedBass;
+
+namespace FredServer
+{
+    class Program
+    {        
+        public static async Task Main()
+        {
+            TcpListener server = null;
+            
+            try
+            {
+                // Set the TcpListener on port 13000.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("192.168.0.108");
+
+                server = new TcpListener(localAddr, port);
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+                                               
+                        // translate data into commands
+                        string[] cmd = data.Split('-');
+                        switch (cmd[0])
+                        {
+                            case "light":
+                                {
+                                    GpioPin light = new GpioPin(21, Direction.Out);
+                                    if (cmd[1] == "on")
+                                    {
+                                        light.Value = PinValue.High;
+                                        Console.WriteLine("Light On");
+                                    }
+                                    if (cmd[1] == "off")
+                                    {
+                                        light.Value = PinValue.Low;
+                                        Console.WriteLine("Light Off");
+                                    }
+                                    break;
+                                }
+                            case "TTS":
+                                {
+                                    await TTS.Speak(cmd[1]);
+                                    Console.WriteLine("FRED says " + cmd[1]);                                    
+                                    FredSays(); 
+                                    Console.ReadLine();
+                                    break;
+                                }
+                        }                       
+       
+                        // Process the data sent by the client.
+                        //data = data.ToUpper();
+
+                        //byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        //stream.Write(msg, 0, msg.Length);
+                        //Console.WriteLine("Sent: {0}", data);
+
+                    } // while
+
+                } // listening loop
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+        } // main   
+
+        public static void FredSays()
+        {            
+            // Init BASS using the default output device
+            if (Bass.Init())
+            {
+                // Create a stream from a file
+                var stream = Bass.CreateStream("fredSays.wav");
+
+                if (stream != 0)
+                    Bass.ChannelPlay(stream); // Play the stream
+
+                // Error creating the stream
+                else Console.WriteLine("Error: {0}!", Bass.LastError);
+
+                // Wait till user presses a key
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+
+                // Free the stream
+                Bass.StreamFree(stream);
+
+                // Free current device.
+                Bass.Free();
+            }
+            
+        }
+    }
+}
---
+++ FredServer/TTS.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FredServer
+{
+    class TTS
+    {
+        public static async Task Speak(string text)
+        {
+            string accessToken;
+
+            Authentication auth = new Authentication("https://eastus.api.cognitive.microsoft.com/sts/v1.0/issueToken", "9423ed79bbda4d3b8e0687363b7d9de2");
+            try
+            {
+                accessToken = await auth.FetchTokenAsync().ConfigureAwait(false);
+                Console.WriteLine("Successfully obtained an access token. \n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to obtain an access token.");
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            string host = "https://eastus.tts.speech.microsoft.com/cognitiveservices/v1";
+
+            string body = @"<speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'>
+              <voice name='Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)'>" +
+              text + "</voice></speak>";
+
+            using (var client = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage())
+                {
+                    // Set the HTTP method
+                    request.Method = HttpMethod.Post;
+                    // Construct the URI
+                    request.RequestUri = new Uri(host);
+                    // Set the content type header
+                    request.Content = new StringContent(body, Encoding.UTF8, "application/ssml+xml");
+                    // Set additional header, such as Authorization and User-Agent
+                    request.Headers.Add("Authorization", "Bearer " + accessToken);
+                    request.Headers.Add("Connection", "Keep-Alive");
+                    // Update your resource name
+                    request.Headers.Add("User-Agent", "TTS");
+                    request.Headers.Add("X-Microsoft-OutputFormat", "riff-24khz-16bit-mono-pcm");
+                    // Create a request
+                    Console.WriteLine("Calling the TTS service. Please wait... \n");
+                    using (var response = await client.SendAsync(request).ConfigureAwait(false))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        // Asynchronously read the response
+                        using (var dataStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                        {
+                            Console.WriteLine("Your speech file is being written to file...");
+                            using (var fileStream = new FileStream(@"fredSays.wav", FileMode.Create, FileAccess.Write, FileShare.Write))
+                            {
+                                await dataStream.CopyToAsync(fileStream).ConfigureAwait(false);
+                                fileStream.Close();
+                            }
+                        }
+                    }
+                }
+            }
+        }// speak
+
+    }
+}
